193ba72db17ebc0a7483d421622e8c66a96b207d
AditiThakur1609 <aditi@dilsayapp.com>
2025-12-13 14:06:26 +0530

    Add root route for Railway

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
-const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT || 3306
-});
+const urlDB = 
+`mysql://${process.env.MYSQLUSER}:
+${process.env.MYSQLPASSWORD}
+@${process.envMYSQLHOST}:
+${process.env.MYSQLPORT}/
+${process.env.MYSQLDATABASE}`
+
+const db = mysql.createConnection(urlDB);
 
 db.connect((err) => {
     if (err) {
